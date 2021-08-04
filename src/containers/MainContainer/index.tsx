43ca6a93884103475e912490d35ae6641ec4fe85
이찬호 <chanho@woowahan.com>
2021-08-04 23:56:27 +0900

    feature. add conatiner

---
+++ src/containers/MainContainer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as S from './style'
+
+const MainContainer: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Header>간호간호</S.Header>
+      <S.Label>간호간호에 오신것을 환영합니다</S.Label>
+    </S.Container>
+  )
+}
+
+export default MainContainer
