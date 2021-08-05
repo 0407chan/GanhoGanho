db23630a4001fde6324e1d17a74227419992f48b
이찬호 <chanho@woowahan.com>
2021-08-05 23:20:55 +0900

    [2] 메인 컨테이너 레이아웃 잡기

--- src/containers/MainContainer/index.tsx
+++ src/containers/MainContainer/index.tsx
@@ -1,13 +1,3 @@
-import React from 'react'
-import * as S from './style'
-
-const MainContainer: React.FC = () => {
-  return (
-    <S.Container>
-      <S.Header>간호간호</S.Header>
-      <S.Label>간호간호에 오신것을 환영합니다</S.Label>
-    </S.Container>
-  )
-}
+import MainContainer from './MainContainer'
 
 export default MainContainer
