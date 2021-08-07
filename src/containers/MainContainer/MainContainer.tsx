7643b1737389c0370f646ab0c8750404de205261
이찬호 <chanho@woowahan.com>
2021-08-07 18:27:29 +0900

    [5] 달력 날짜 옮기는 기능 추가

--- src/containers/MainContainer/MainContainer.tsx
+++ src/containers/MainContainer/MainContainer.tsx
@@ -1,22 +1,40 @@
 import Calendar from '@/components/Calendar'
-import { format } from 'date-fns'
-import React from 'react'
+import LeftOutlined from '@ant-design/icons/LeftOutlined'
+import RightOutlined from '@ant-design/icons/RightOutlined'
+import { addMonths, subMonths } from 'date-fns'
+import format from 'date-fns/format'
+import React, { useState } from 'react'
 import * as S from './style'
 const MainContainer: React.FC = () => {
+  const [currentDate, setCurrentDate] = useState<Date>(new Date())
+
+  const onPrevMonth = () => {
+    setCurrentDate(subMonths(currentDate, 1))
+  }
+  const onNextMonth = () => {
+    setCurrentDate(addMonths(currentDate, 1))
+  }
+  const onToday = () => {
+    setCurrentDate(new Date())
+  }
   return (
     <S.Container>
       <S.HeaderWrapper>
         <S.HeaderLeft>간호간호</S.HeaderLeft>
         <S.HeaderCenter>
-          {format(new Date(), 'M')}
-          <span>월 근무표</span>
+          <div>
+            <S.CalendarButton icon={<LeftOutlined />} onClick={onPrevMonth} />
+            {format(currentDate, 'yyyy년 M월')}
+            <S.CalendarButton icon={<RightOutlined />} onClick={onNextMonth} />
+          </div>
+          <S.CalendarButton onClick={onToday}>오늘</S.CalendarButton>
         </S.HeaderCenter>
         <S.HeaderLeft />
       </S.HeaderWrapper>
       <S.ContentWrapper>
         <S.ContentLeft>간호간호</S.ContentLeft>
         <S.ContentCenter>
-          <Calendar />
+          <Calendar currentDate={currentDate} />
         </S.ContentCenter>
         <S.ContentLeft>오른쪽</S.ContentLeft>
       </S.ContentWrapper>
