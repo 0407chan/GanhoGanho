0b15ad3807dc5b0bf395879fb58e92ca4910b26f
이찬호 <chanho@woowahan.com>
2021-08-09 23:47:23 +0900

    [8] MainContainer에 있던 헤더 컴포넌트로 추출
    - 사이드 매니져 추가
    - 에디터 추가
    - 레이아웃은 각 컴포넌으로 위임

--- src/containers/MainContainer/MainContainer.tsx
+++ src/containers/MainContainer/MainContainer.tsx
@@ -1,12 +1,23 @@
 import Calendar from '@/components/Calendar'
-import LeftOutlined from '@ant-design/icons/LeftOutlined'
-import RightOutlined from '@ant-design/icons/RightOutlined'
+import Editor from '@/components/Editor'
+import Header from '@/components/Header'
+import SideManage from '@/components/SideManage'
 import { addMonths, subMonths } from 'date-fns'
-import format from 'date-fns/format'
 import React, { useState } from 'react'
 import * as S from './style'
+
+export type User = {
+  name: string
+  offDate: Date[]
+}
+
 const MainContainer: React.FC = () => {
   const [currentDate, setCurrentDate] = useState<Date>(new Date())
+
+  const [currentUser, setCurrentUser] = useState<User>({
+    name: '피카츄',
+    offDate: []
+  })
 
   const onPrevMonth = () => {
     setCurrentDate(subMonths(currentDate, 1))
@@ -19,24 +30,16 @@ const MainContainer: React.FC = () => {
   }
   return (
     <S.Container>
-      <S.HeaderWrapper>
-        <S.HeaderLeft>간호간호</S.HeaderLeft>
-        <S.HeaderCenter>
-          <div>
-            <S.CalendarButton icon={<LeftOutlined />} onClick={onPrevMonth} />
-            {format(currentDate, 'yyyy년 M월')}
-            <S.CalendarButton icon={<RightOutlined />} onClick={onNextMonth} />
-          </div>
-          <S.CalendarButton onClick={onToday}>오늘</S.CalendarButton>
-        </S.HeaderCenter>
-        <S.HeaderLeft />
-      </S.HeaderWrapper>
+      <Header
+        onNextMonth={onNextMonth}
+        onPrevMonth={onPrevMonth}
+        onToday={onToday}
+        currentDate={currentDate}
+      />
       <S.ContentWrapper>
-        <S.ContentLeft>간호간호</S.ContentLeft>
-        <S.ContentCenter>
-          <Calendar currentDate={currentDate} />
-        </S.ContentCenter>
-        <S.ContentLeft>오른쪽</S.ContentLeft>
+        <SideManage currentUser={currentUser} setCurrentUser={setCurrentUser} />
+        <Calendar currentDate={currentDate} />
+        <Editor currentUser={currentUser} />
       </S.ContentWrapper>
     </S.Container>
   )
