f4b120bae97448a5b3335a97c00d9b9d2a0d939f
이찬호 <chanho@woowahan.com>
2021-08-09 23:47:38 +0900

    [8] 헤더, 컨텐츠 관련 스타일 삭제

--- src/containers/MainContainer/style.tsx
+++ src/containers/MainContainer/style.tsx
@@ -1,4 +1,3 @@
-import Button from 'antd/lib/button'
 import styled from 'styled-components'
 
 export const Container = styled.div`
@@ -10,80 +9,11 @@ export const Container = styled.div`
   align-items: center;
 `
 
-export const HeaderWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 200px calc(100% - 400px) 200px;
-  width: 100%;
-  height: 64px;
-`
-export const HeaderLeft = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  &:hover {
-    background-color: #666871;
-  }
-`
-export const HeaderCenter = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  color: #eeeeee;
-  font-size: 24px;
-  font-weight: bold;
-
-  &:hover {
-    background-color: #666871;
-  }
-`
-
-export const CalendarButton = styled(Button)``
-
-export const HeaderRight = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  &:hover {
-    background-color: #666871;
-  }
-`
-
 export const ContentWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 200px calc(100% - 400px) 200px;
-  width: 100%;
-  height: 100%;
-`
-export const ContentLeft = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: row;
   width: 100%;
   height: 100%;
-  &:hover {
-    background-color: #666871;
-  }
-`
-export const ContentCenter = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-`
-export const ContentRight = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  &:hover {
-    background-color: #666871;
-  }
 `
 
 export const Label = styled.div`
